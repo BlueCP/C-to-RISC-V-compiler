38ee6c866017f2f0c49801700e9b3cd4cec754a7
mxwlc <maxwellcorey85@gmail.com>
2023-03-23 20:45:25 +0000

    TODO: global if and dereferencing global v

--- src/ast/expressions/unary_expr/addr_of_expr.cpp
+++ src/ast/expressions/unary_expr/addr_of_expr.cpp
@@ -1,8 +1,18 @@
 #include "ast/expressions/unary_expr/addr_of_expr.hpp"
 
-AddrOfExpr::AddrOfExpr(Node* e) : UnaryOp(e) {}
+AddrOfExpr::AddrOfExpr(Node *e) : UnaryOp(e) {}
 
-void AddrOfExpr::compile(std::ostream& os, int dest_reg, Context& context) const {
+void AddrOfExpr::compile(std::ostream &os, int dest_reg, Context &context) const
+{
     // TODO
-
+    if (1 /*global*/)
+    {
+        os << "lui " << reg_name[dest_reg] << ", %hi(" << expr->identifier << ")" << std::endl;
+        os << "addi " << reg_name[dest_reg] << ", " << reg_name[dest_reg] << ",%lo(" << expr->identifier << ")" << std::endl;
+    }
+    else
+    {
+        int offset = context.find_fp_offset(expr->identifier);
+        os << "addi " << reg_name[dest_reg] << ", fp, -" << offset << std::endl;
+    }
 }
--- src/ast/expressions/unary_expr/deref_expr.cpp
+++ src/ast/expressions/unary_expr/deref_expr.cpp
@@ -4,4 +4,10 @@ DerefExpr::DerefExpr(Node* e) : UnaryOp(e) {}
 
 void DerefExpr::compile(std::ostream& os, int dest_reg, Context& context) const {
     // TODO
+    if(/*global*/1){
+        //not sure see here x
+    }else{
+        int offset = context.find_fp_offset(expr->identifier);
+        os << "lw " << reg_name[dest_reg] << ", -" << offset << "(fp)" << std::endl;
+    }
 }
